49bbda1fd07cdbe61d6656263a45e49e4ecb8637
yoni206 <yoni206@gmail.com>
2019-01-28 01:23:08 -0800

    working on script

---
+++ scripts/analyze_cluster_ic_results.py
@@ -0,0 +1,58 @@
+import pandas as ps
+import sys
+import os
+
+def main(results_dir, output_file):
+    results = {}
+    results_dirs = [d for d in os.listdir(results_dir)]
+    for d in results_dirs:
+        config = d
+        directory = results_dir + "/" + d
+        results[config] = {}
+        err_files = [f for f in os.listdir(directory) if f.endswith(".err") and not f.startswith(".")]
+        for err_file in err_files:
+            log_file = err_file.replace(".smt2.err", ".smt2.log")
+            smt_file = err_file.replace(".smt2.err", ".smt2")
+            with open(directory + "/" + err_file, "r") as myfile:
+                err_content = myfile.read()
+            with open(directory + "/" + log_file, "r") as myfile:
+                log_content = myfile.read()
+            status = get_status(err_content)
+            is_ok = get_status_ok(status)
+            if is_ok:
+                result = get_result(log_content)
+            else:
+                result = "no result"
+            results[config + "/" + smt_file] = status + "," + result
+    print(results)
+    df = ps.DataFrame(list(results.items()))
+    df.to_csv(output_file)
+
+def get_status(err_content):
+    lines = err_content.splitlines()
+    prefix = "[runlim] status:"
+    status_lines = [line for line in lines if line.startswith(prefix)]
+    assert(len(status_lines) == 1)
+    status_line = status_lines[0]
+    status = status_line[len(prefix):].strip()
+    return status
+
+def get_status_ok(status):
+    return status == "ok"
+
+def get_result(log_content):
+    lines = log_content.splitlines()
+    bad_prefix = "c"
+    good_lines = [l for l in lines if not l.startswith(bad_prefix)]
+    assert(len(good_lines) == 1)
+    good_line = good_lines[0]
+    return good_line
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print('arg1: cluster results dir\narg2: output file\n')
+        exit(1)
+    results_dir = sys.argv[1]
+    output_file = sys.argv[2]
+    main(results_dir, output_file)
