26d89883b1a401e0814149913e93c7910d078433
Yoni Zohar <yoni206@gmail.com>
2019-01-27 21:09:53 -0800

    changes from cluster

---
+++ rewriting/cluster_results/cvc4_tplanes_cbqi/script.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#SBATCH -e /dev/null
+#SBATCH -o /dev/null
+#SBATCH -c 1
+#SBATCH -a 1-160
+#SBATCH --qos=normal
+#SBATCH -t 00:00:3600
+#SBATCH -D /barrett/scratch/yoniz/git/invertibility/rewriting/cluster_results/cvc4_tplanes_cbqi
+
+prefix="/barrett/scratch/yoniz/git/invertibility/rewriting/bms/"
+runlim_binary="/barrett/scratch/local/bin/runlim"
+runlim_options="--time-limit=3600 --space-limit=4000"
+solver="./cvc4"
+solver_options="--nl-ext-tplanes --cbqi-all"
+benchmarks="/barrett/scratch/yoniz/git/invertibility/rewriting/cluster_results/cvc4_tplanes_cbqi/benchmarks"
+scrambler=""
+
+# pick benchmark file from list of benchmarks
+benchmark="`sed ${SLURM_ARRAY_TASK_ID}'q;d' $benchmarks`"
+out="$(echo ${benchmark#$prefix} | sed -e 's,/,-,g')"
+# set stdout log file
+log="/barrett/scratch/yoniz/git/invertibility/rewriting/cluster_results/cvc4_tplanes_cbqi/${out}.log"
+# set stderr log file
+err="/barrett/scratch/yoniz/git/invertibility/rewriting/cluster_results/cvc4_tplanes_cbqi/${out}.err"
+
+(
+filename="$(basename $benchmark)"
+extension="${filename##*.}"
+[ -n "$extension" ] && extension=".$extension"
+benchmark_tmp="$(mktemp --suffix="$extension" /tmp/XXXXXXXX)"
+
+echo "c host: $(hostname)"
+echo "c start: $(date)"
+echo "c benchmark: $benchmark"
+echo "c solver: $solver"
+echo "c solver options: $solver_options"
+
+# copy benchmark from NFS to /tmp
+cp "$benchmark" "$benchmark_tmp"
+
+# scramble benchmark if scrambler is given
+if [ -e "$scrambler" ]; then
+  benchmark_tmp_scrambled="${benchmark_tmp}_scrambled"
+  $scrambler "$benchmark_tmp"  > \
+    "$benchmark_tmp_scrambled"
+  mv "$benchmark_tmp_scrambled" "$benchmark_tmp"
+fi
+
+$runlim_binary $runlim_options "$solver" $solver_options "$benchmark_tmp"
+
+# remove benchmark from /tmp
+rm -f "$benchmark_tmp"
+
+echo "c done"
+) > "$log" 2> "$err"
---
+++ rewriting/submit-jobs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+exp_base_dir="/barrett/scratch/yoniz/git/invertibility/rewriting"
+cvc4_binary="/barrett/scratch/yoniz/git/CVC4/build/bin/cvc4"
+z3_binary="/barrett/scratch/yoniz/git/z3/build/z3"
+partition="normal"         # default
+num_cores=1
+space_limit="4000"
+
+benchmark_set=foo
+time_limit=3600
+subdir_name=cluster_results
+
+#
+# cvc4 runs
+#
+directory_name="$exp_base_dir/$subdir_name/cvc4_default"
+options=""
+echo -e "$benchmark_set\n$directory_name\n$options\n$partition\n$time_limit\n$space_limit\n$num_cores\n" | submit-solver.sh $cvc4_binary
+
+directory_name="$exp_base_dir/$subdir_name/cvc4_tplanes"
+options="--nl-ext-tplanes"
+echo -e "$benchmark_set\n$directory_name\n$options\n$partition\n$time_limit\n$space_limit\n$num_cores\n" | submit-solver.sh $cvc4_binary
+
+directory_name="$exp_base_dir/$subdir_name/cvc4_tplanes_fmf"
+options="--nl-ext-tplanes --fmf-fun-rlv"
+echo -e "$benchmark_set\n$directory_name\n$options\n$partition\n$time_limit\n$space_limit\n$num_cores\n" | submit-solver.sh $cvc4_binary
+
+directory_name="$exp_base_dir/$subdir_name/cvc4_tplanes_cbqi"
+options="--nl-ext-tplanes --cbqi-all"
+echo -e "$benchmark_set\n$directory_name\n$options\n$partition\n$time_limit\n$space_limit\n$num_cores\n" | submit-solver.sh $cvc4_binary
+
+directory_name="$exp_base_dir/$subdir_name/cvc4_cbqi"
+options="--cbqi-all"
+echo -e "$benchmark_set\n$directory_name\n$options\n$partition\n$time_limit\n$space_limit\n$num_cores\n" | submit-solver.sh $cvc4_binary
+
+directory_name="$exp_base_dir/$subdir_name/cvc4_fmf"
+options="--fmf-fun-rlv"
+echo -e "$benchmark_set\n$directory_name\n$options\n$partition\n$time_limit\n$space_limit\n$num_cores\n" | submit-solver.sh $cvc4_binary
+
+#
+# z3 runs
+#
+directory_name="$exp_base_dir/$subdir_name/z3_default"
+options=""
+echo -e "$benchmark_set\n$directory_name\n$options\n$partition\n$time_limit\n$space_limit\n$num_cores\n" | submit-solver.sh $z3_binary
+#
